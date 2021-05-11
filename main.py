df8c223586b381f4b02742e718820b6d0a9c390f
qaws5503 <57092368+qaws5503@users.noreply.github.com>
2021-05-11 20:56:07 +0800

    Update arguments

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import stock_flask
 from argparse import ArgumentParser
 # %%
 parser = ArgumentParser()
-parser.add_argument("-m", "--mode", help="select mode, default=0. Not using flask is 0, using flask is 1.", default=0)
-parser.add_argument("-s", "--save", help="save as html or not, default=0. If you want to save set to 1.", default=0)
+parser.add_argument("-m", "--mode", help="select mode, default=0. Using Flask just set to 1.", default=0)
+parser.add_argument("-s", "--save", help="save as html or not, default=0. If you want to save html files set to 1.", default=0)
 args = parser.parse_args()
 if args.mode == 0:
     save_html = bool(args.save)
